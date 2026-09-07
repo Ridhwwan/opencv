998a9f60ac47bcb692c3929bdc91901506dff7e9
unknown <salaudeenridhwan@gmail.com>
2026-09-07 18:13:42 +0100

    Initial commit

---
+++ demo.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read an image
+def readImage():
+    root = os.getcwd()
+    imgPath = os.path.join(root,'storage\\images\\lampbg.png')
+    img = cv.imread(imgPath)
+    debug = 1
+    cv.imshow ('img', img)
+    cv.waitKey(0) 
+    # 0 is a delay time in milliseconds. 0 means wait indefinitely until a key is pressed
+
+
+# write an image
+def writeImage():
+    root = os.getcwd()
+    imgPath = os.path.join(root,'storage\\images\\lampbg.png')
+    img = cv.imread(imgPath)
+    outPath = os.path.join(root,'storage\\lampbgoutput.png')
+    # if image with the same name exists, it will be overwritten
+    cv.imwrite(outPath, img)
+
+
+
+
+    
+if __name__ == '__main__':
+    # readImage()
+    writeImage()
